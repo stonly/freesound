11d98ed991813ff532f23bf107873ddc7e8681c4
Stonly Baptiste <stonlyb@gmail.com>
2013-11-17 15:47:21 -0500

    initial

---
+++ js/controllers.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/* Controllers */
+
+angular.module('myApp.controllers', []).
+  controller('MyCtrl1', ['$http', function($http) {
+//fca4cde51acf4dd4ab631b8ca79fa2e7
+//http://www.freesound.org/api/sounds/search?q=barking&api_key=12d6dc5486554e278e370cdc49935908
+
+     $http({method: 'GET', url: 'http://www.freesound.org/api/sounds/search', params: {q: 'barking', api_key: 'fca4cde51acf4dd4ab631b8ca79fa2e7', headers: {'Access-Control-Allow-Origin': '*'}}}, function(data, status) {
+	console.log(data, status);
+     });
+  }])
+  .controller('MyCtrl2', [function() {
+
+  }]);
