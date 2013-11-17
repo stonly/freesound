9fee2f79bd628756ad64a511230882aa80e06d88
Stonly Baptiste <stonlyb@gmail.com>
2013-11-17 17:47:41 -0500

    working

--- js/controllers.js
+++ js/controllers.js
@@ -3,13 +3,18 @@
 /* Controllers */
 
 angular.module('myApp.controllers', []).
-  controller('MyCtrl1', ['$http', function($http) {
-//fca4cde51acf4dd4ab631b8ca79fa2e7
-//http://www.freesound.org/api/sounds/search?q=barking&api_key=12d6dc5486554e278e370cdc49935908
-delete $http.defaults.headers.common['X-Requested-With'];
-     $http({method: 'GET', url: 'http://www.freesound.org/api/sounds/search', params: {q: 'barking', api_key: 'fca4cde51acf4dd4ab631b8ca79fa2e7', headers: {'Access-Control-Allow-Origin': '*'}}}, function(data, status) {
+  controller('MyCtrl1', ['$scope', '$http', function($scope, $http) {
+     delete $http.defaults.headers.common['X-Requested-With'];
+     $scope.sounds = [];
+
+     $http({method: 'GET', url: 'http://www.freesound.org/api/sounds/search/', params: {q: 'barking', api_key: 'fca4cde51acf4dd4ab631b8ca79fa2e7'}}).success(function(data, status) {
 	console.log(data, status);
+        if(data.sounds && data.sounds.length) {
+            $scope.sounds = data.sounds;
+        }
+     }).error(function(data, status) {
      });
+
   }])
   .controller('MyCtrl2', [function() {
 
